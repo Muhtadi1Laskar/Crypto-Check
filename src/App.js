93e03f1ac1545f01cf741217d1ee718f425a0b22
Muhtadi <laskar90muhtadi@gmail.com>
2020-05-20 03:59:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Table, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
+  const [bitdata, setBitdata] = useState([]);
+
+  async function callApi() {
+    const res = await fetch("https://api.coinlore.net/api/tickers/");
+    res
+      .json()
+      .then((res) => {
+        setBitdata(res.data);
+      })
+      .then((err) => err);
+  }
+
+  useEffect(() => {
+    callApi();
+  }, []);
+
+  const renderTags = () => {
+    let body = bitdata.map((r) => {
+      return (
+        <tr>
+          <td>{r.rank}</td>
+          <td className="td-name">
+            {r.name} <span>{r.symbol}</span>
+          </td>
+          <td>$ {r.price_usd}</td>
+          <td>$ {r.market_cap_usd}</td>
+          <td className="td-percent">{r.percent_change_24h}%</td>
+          <td className="td-percent">{r.percent_change_1h}%</td>
+          <td>{r.volume24}</td>
+        </tr>
+      );
+    });
+    return (
+      <Table responsive striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Price USD</th>
+              <th>Market Cap</th>
+              <th>Percent Change 24hr</th>
+              <th>Percent Change 1hr</th>
+              <th>Volume 24</th>
+            </tr>
+          </thead>
+          <tbody>{body}</tbody>
+        </Table>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="Row">
+        <h1>Crypto Check</h1>
+      </Row>
+      <Row>
+        {renderTags()}
+      </Row>
+    </Container>
   );
 }
 
